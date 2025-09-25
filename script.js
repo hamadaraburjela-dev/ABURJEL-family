6f63371d822fdc5ead23af19594cc0feac000eec
hamada2210m-pixel <hamada2210m@gmail.com>
2025-09-25 15:09:52 +0300

    طيب

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const App = {
-        WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxmj1Rd78NiPuPJIyzz9BxgHrAr-CaO8bPSwoQ3TkfXjdIPC5mTQsxZYBaWlSJCPH9pPw/exec', // سيتم تحديثه بالرابط الجديد من Google Apps Script
+        WEB_APP_URL: 'https://script.google.com/macros/s/AKfycby4uunZIRhd_dT5OFSkpoYEL0kwrPFRyR9qCFmPg8lwTGLqfXxp3xPDv-tocml2_SRGWg/exec', // سيتم تحديثه بالرابط الجديد من Google Apps Script
         
         async testNewUrl() {
             const input = document.getElementById('newScriptUrl');
